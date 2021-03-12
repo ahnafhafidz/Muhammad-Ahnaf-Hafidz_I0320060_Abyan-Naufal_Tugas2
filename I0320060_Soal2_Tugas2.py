976b473eaa8bb16ca91d178355f4dd103a2735d9
Ahnaf Hafidz <ahnaf.muhammad19@gmail.com>
2021-03-12 18:42:32 +0700

    Soal2

---
+++ I0320060_Soal2_Tugas2.py
@@ -0,0 +1,39 @@
+print("Praktikum Programa Komputer week 2")
+
+print("Soal 2")
+
+print("Identitas Pribadi")
+
+#memasukan variabel biodata
+
+nama = "Muhammad Ahnaf Hafidz"
+nama_panggilan = "Ahnaf"
+agama = "Islam"
+tempat_lahir = "Yogyakarta"
+tanggal_lahir = "24 Juli 2002"
+alamat = "Wirobrajan WB2/161, Yogyakarta"
+umur = 18
+tinggi_badan = 165
+berat_badan = 55.5
+golongan_darah = "B"
+universitas = "Universitas Sebelas Maret"
+prodi = "Teknik Industri"
+nim = "I0320060"
+ukuran_sepatu = 42
+hobi = "Mendaki Gunung"
+
+#menampilkan variabel ke terminal
+
+print("Nama saya", nama)
+print("Biasa dipanggil", nama_panggilan)
+print("Saya beragama", agama)
+print("Saya lahir di", tempat_lahir, "pada", tanggal_lahir)
+print("Rumah saya beralamat di", alamat)
+print("Saya berumur", umur, "tahun")
+print("Tinggi badan saya", tinggi_badan, "cm")
+print("Berat badan saya", berat_badan, "Kg")
+print("Saya begolongan darah", golongan_darah)
+print("Saya kuliah di", universitas, "di prodi", prodi)
+print("NIM saya", nim)
+print("Saya biasa memakai sepatu berukuran", ukuran_sepatu)
+print("Saya suka", hobi)
